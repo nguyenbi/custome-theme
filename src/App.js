0385c95ea5240ce4ce006e0bcae412fd024ca65c
nguyenbi <nguyenbi.it@gmail.com>
2023-01-14 05:33:06 +0700

    Set color picker

--- src/App.js
+++ src/App.js
@@ -5,17 +5,40 @@ import Reset from "./components/Reset";
 import FontSizeSetting from "./components/FontSizeSetting";
 import Result from "./components/Result";
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: 'red',
+      fontSize: 15
+    }
+    // this.onSetColor = this.onSetColor.bind(this);
+  }
+
+  // onSetColor(param) {
+  //   this.setState({
+  //     color: param
+  //   });
+  // }
+
+  onSetColor = (param) => {
+    this.setState(
+      {
+        color: param
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container mt-50">
           <div className="row">
-            <ColorPicker />
+            <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor} />
             <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
               <FontSizeSetting />
               <Reset />
             </div>
-            <Result />
+            <Result color={this.state.color} />
           </div>
         </div>
       </div>
--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
 class ColorPicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            colors: ['red', 'green', 'yellow', 'blue']
+        }
+    }
+    showColor(item) {
+
+        return item === "yellow" ? {
+            backgroundColor: item,
+            color: "#000"
+        } :
+            {
+                backgroundColor: item,
+                color: "#fff"
+            }
+    }
+
+    setActiveColor(param) {
+        this.props.onReceiveColor(param)
+    }
     render() {
+        var elementColors = this.state.colors.map((item, index) => {
+            return (
+                <span
+                    key={index}
+                    style={this.showColor(item)}
+                    className={this.props.color === item ? "active" : ""}
+                    onClick={() => this.setActiveColor(item)}
+                >{item}</span>
+            )
+        })
         return (
             <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                 <div className="panel panel-primary">
@@ -8,11 +39,12 @@ class ColorPicker extends Component {
                         <h3 className="panel-title">Color Picker</h3>
                     </div>
                     <div className="panel-body">
-                        <span></span>
+                        {elementColors}
+                        <hr />
                     </div>
                 </div>
 
-            </div>
+            </div >
         );
     }
 }
